1db03f1f4072346c3606d5e9be5f8f0c7a3db234
jltemprano@gmail.com <jltemprano@gmail.com>
2020-03-11 17:51:13 +0100

    Modificado, faltan las vistas

---
+++ helpdesk-curso/__manifest__.py
@@ -0,0 +1,15 @@
+{
+"name":  'Help Desk Curso',
+'summary': 'Module to support teams ',
+'version': '13.0.1.0.0',
+'category':'Customer Relationship Management',
+'website':'',
+'author': 'JL Temprano',
+'license': 'AGPL-3',
+'data': [
+    'security/helpdesk_security.xml',
+    'views/helpdesk_ticket_views.xml'
+],
+'application': True,
+'installable': True,
+},
